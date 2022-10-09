f12a81b3570d39614e36f8edf2b2909e9464067b
WOSAJ <wosajmine312213@gmail.com>
2022-10-09 17:28:18 +1000

    101% content from 1.16.5!

---
+++ src/main/java/com/wosaj/zenhorizon/test/GameTests.java
@@ -0,0 +1,11 @@
+package com.wosaj.zenhorizon.test;
+
+import net.minecraftforge.gametest.GameTestHolder;
+
+import static net.minecraft.Util.NIL_UUID;
+
+@GameTestHolder
+public class GameTests {
+
+    private GameTests(){}
+}
